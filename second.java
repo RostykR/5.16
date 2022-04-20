4c32f118c2626eda801fa3fbfdb77094c9ce4aa2
RostykR <99317303+RostykR@users.noreply.github.com>
2022-04-20 18:28:34 +0300

    Add files via upload

---
+++ second.java
@@ -0,0 +1,11 @@
+public class second {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        double c = Math.sqrt((a*a) + (b*b));
+        int pl = (a*b)/2;
+        double per = a + b + c;
+        System.out.println("Периметр  трикутника: " + per);
+        System.out.println("Площа трикутника: " + pl);
+    }
+}
---
+++ third.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class third {
+    public static void main(String[] args) {
+        double num = Math.random()*102;
+        System.out.println(num);
+        System.out.println(String.valueOf(num).length());
+    }
+}
